c19cda3356ae04a63c3e2121e9beab56f0305f18
maen2020 <maen210212@gmail.com>
2021-07-06 20:37:39 -0500

    Implementando Launch Screan

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'src/my_app.dart';
+
 void main() {
   runApp(MyApp());
 }
--- lib/src/my_app.dart
+++ lib/src/my_app.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({Key key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
